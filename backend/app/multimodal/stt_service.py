41b232ed2d902b3f3f76dea571f8f9d943b92919
l247711-coder <l247711@lhr.nu.edu.pk>
2026-02-27 13:01:40 +0500

    update stt_service.py
    update stt_service.py to convert  incoming audio to raw PCM WAV format before giving to Whisper.

--- backend/app/multimodal/stt_service.py
+++ backend/app/multimodal/stt_service.py
@@ -1,3 +1,4 @@
+'''version1
 from faster_whisper import WhisperModel
 
 
@@ -26,5 +27,66 @@ class STTService:
         text = ""
         for segment in segments:
             text += segment.text + " "
+
+        return text.strip()
+
+'''
+
+#Version 2
+# app/multimodal/stt_service.py
+
+from faster_whisper import WhisperModel
+import subprocess
+import tempfile
+import os
+
+
+class STTService:
+
+    def __init__(self):
+
+        print("Loading Whisper medium on GPU...")
+
+        self.model = WhisperModel(
+            "medium",
+            device="cuda",
+            compute_type="float16"
+        )
+
+        print("Whisper ready.")
+
+    def convert_to_wav(self, input_path):
+
+        output_path = input_path.replace(".webm", ".wav")
+
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i", input_path,
+            "-ar", "16000",
+            "-ac", "1",
+            output_path
+        ]
+
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        return output_path
+
+    def transcribe_audio(self, audio_path):
+
+        # Convert to WAV first
+        wav_path = self.convert_to_wav(audio_path)
+
+        segments, info = self.model.transcribe(
+            wav_path,
+            beam_size=5,
+            vad_filter=True
+        )
+
+        text = ""
+        for segment in segments:
+            text += segment.text + " "
+
+        os.remove(wav_path)
 
         return text.strip()
